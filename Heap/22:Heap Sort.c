31cff0536f232fc0c2e3f8c37cf467199be92a66
Zane-Dev14 <ericatkusa@gmail.com>
2023-12-14 20:27:31 +0530

    Done

--- Heap/22:Heap Sort.c
+++ Heap/22:Heap Sort.c
@@ -1,41 +1,4 @@
-import os
-all='''Polynomial Sum Program(List)
-Matrix List
-Circular Queue 
-Array Queue 
-Stack Array 
-Priority Queue Array
-Double-Ended Queue Array
-Infix to Postfix(Stack)
-Postfix evaluation(Stack)
-Linked List 
-Stack Linked List
-Queue Linked List
-Reverse Queue Content
-Polynomial Linked List
-Student List 
-Doubly Linked List
-Binary Tree Operations
-Binary Search Tree
-Sort with Binary Tree
-Graph Search Operations
-File Sorting Operations
-Heap Sort
-Hash Table Chaining
-Hash Table Linear Probing'''
-questions :str =[ f"{index+1}:{i}" for index,i in enumerate(all.split("\n"))] 
-stack :str=[i for i in questions if ("Stack" in i)or("Queue" in i)]
-Linked :str=[i for i in questions if "List" in i]
-heap:str=[i for i in questions if "Heap" in i]
-tree:str=[i for i in questions if ("Operations" in i)or("Tree" in i)]
-for i in heap:
-    # Specify the file path
-    file_path = f"/home/zane/Desktop/Code/git/DS Lab/Heap/{i}.c"
-    # Create an empty file using os.mknod()
-    try:
-        
-        with open(file_path,"w") as fa:
-            code='''#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 // Function prototypes for binary heap
@@ -54,12 +17,12 @@ int main() {
 
     do {
         // Display menu
-        printf("\\nMenu:\\n");
-        printf("1. Insert into Heap\\n");
-        printf("2. Extract Min from Heap\\n");
-        printf("3. Display Heap\\n");
-        printf("4. Quick Sort from File\\n");
-        printf("5. Exit\\n");
+        printf("\nMenu:\n");
+        printf("1. Insert into Heap\n");
+        printf("2. Extract Min from Heap\n");
+        printf("3. Display Heap\n");
+        printf("4. Quick Sort from File\n");
+        printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -73,9 +36,9 @@ int main() {
 
             case 2:
                 if (heap_size > 0) {
-                    printf("Extracted Min: %d\\n", extractMin(heap, &heap_size));
+                    printf("Extracted Min: %d\n", extractMin(heap, &heap_size));
                 } else {
-                    printf("Heap is empty.\\n");
+                    printf("Heap is empty.\n");
                 }
                 break;
 
@@ -91,11 +54,11 @@ int main() {
                 break;
 
             case 5:
-                printf("Exiting the program.\\n");
+                printf("Exiting the program.\n");
                 break;
 
             default:
-                printf("Invalid choice. Please enter a valid option.\\n");
+                printf("Invalid choice. Please enter a valid option.\n");
         }
 
     } while (choice != 5);
@@ -125,8 +88,3 @@ void quickSortFile(char* file_path) {
 }
 
 
-'''
-            fa.write(code)
-        print(f"File '{file_path}' created successfully.")
-    except FileExistsError:
-        print(f"File '{file_path}' already exists.")
